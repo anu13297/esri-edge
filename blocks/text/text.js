e1dd8fadca1fa3902da8b1a5fa206840ee1caf9d
anu13297 <amallempati@esri.com>
2024-07-25 15:19:27 -0400

    esri text

---
+++ blocks/text/text.js
@@ -0,0 +1,21 @@
+import { decorateIcons } from '../../scripts/aem.js';
+import { span, ul, li, domEl, div } from '../../scripts/dom-helpers.js';
+
+export default function decorate(block) {
+
+    block.classList.add('esri-text');
+
+    const list = ul(...[...block.children].map((row) => li(...[...row.children].map((divElement) => {
+        if (
+            divElement.children.length === 1
+            && divElement.querySelector('picture')
+        ) {
+            divElement.className = 'esritext-image';
+        } else divElement.className = 'esritext-body';
+
+        return divElement;
+    }))));
+    
+    block.textContent = '';
+    block.append(list);
+}
