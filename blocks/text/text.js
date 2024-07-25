a0307c6b220c246f13fca42cf9f76acd94bf8ed2
anu13297 <amallempati@esri.com>
2024-07-25 15:21:25 -0400

    updated text js file

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,21 +1,18 @@
-import { decorateIcons } from '../../scripts/aem.js';
-import { span, ul, li, domEl, div } from '../../scripts/dom-helpers.js';
+import { ul, li } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
+  block.classList.add('esri-text');
+  const list = ul(...[...block.children].map((row) => li(...[...row.children].map((divElement) => {
+    if (
+      divElement.children.length === 1
+      && divElement.querySelector('picture')
+    ) {
+      divElement.className = 'esritext-image';
+    } else divElement.className = 'esritext-body';
 
-    block.classList.add('esri-text');
+    return divElement;
+  }))));
 
-    const list = ul(...[...block.children].map((row) => li(...[...row.children].map((divElement) => {
-        if (
-            divElement.children.length === 1
-            && divElement.querySelector('picture')
-        ) {
-            divElement.className = 'esritext-image';
-        } else divElement.className = 'esritext-body';
-
-        return divElement;
-    }))));
-    
-    block.textContent = '';
-    block.append(list);
+  block.textContent = '';
+  block.append(list);
 }
