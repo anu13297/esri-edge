24d11f9deb2ae0a807aae99af60948e86b1acbbe
anu13297 <amallempati@esri.com>
2024-09-09 10:03:52 -0400

    cards standard updates

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,12 +3,11 @@ import { domEl } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   block.classList.add('calcite-mode-dark');
+  block.classList.add('cardsperrow');
 
   const processSimpleCard = (div) => {
-    if (!block.classList.contains('simple')) {
-      return;
-    }
-    block.classList.add('cardsperrow');
+    if (!block.classList.contains('simple')) return;
+    
     const anchorEl = div.querySelector('a');
     const cardBodyContent = domEl('div', { class: 'card-body-content' });
     if (anchorEl) {
@@ -24,10 +23,8 @@ export default function decorate(block) {
   };
 
   const processStandardCard = (div) => {
-    if (!block.classList.contains('standard')) {
-      return;
-    }
-    block.classList.add('cardsperrow');
+    if (!block.classList.contains('standard')) return;
+
     const anchorEl = div.querySelector('a');
     const cardBodyContent = domEl('div', { class: 'card-body-content' });
     if (anchorEl) {
@@ -38,14 +35,23 @@ export default function decorate(block) {
     }
     div.querySelectorAll('p').forEach((p) => { if (p.textContent === '') p.remove(); });
     [...div.querySelectorAll('.cards-card-body > :not(.card-body-content, a)')].forEach((el) => {
-      cardBodyContent.append(el);
+     cardBodyContent.append(el);
       if (el.tagName === 'P' && el.children.length === 0 && el.parentNode.firstElementChild === el) {
         el.classList.add('overlay-text');
       }
     });
     const pictureEl = div.querySelector('picture').closest('p');
     const overlayTextEl = div.querySelector('.overlay-text');
+    
     if (overlayTextEl) pictureEl.append(overlayTextEl);
+    pictureEl.nextElementSibling.classList.add('card-body-title');
+    const cardBodyTitle = div.querySelector('.card-body-title');
+
+    if(cardBodyContent.lastChild.classList == '') cardBodyContent.lastChild.classList.add('card-body-description');
+
+    if (cardBodyTitle.nextElementSibling && !cardBodyTitle.nextElementSibling.classList.contains('card-body-description')) {
+        cardBodyTitle.nextElementSibling.classList.add('card-body-subtitle');
+      }
   };
 
   /* change to ul, li */
